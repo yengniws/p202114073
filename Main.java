cbf424f15f0db5fbe1600f91a83f02fea1593002
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-11-20 09:09:43 +0900

    two

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
       //  자신의 학번과 이름을 출력하라
       System.out.println("202114073 박예은");
+      System.out.println(Hello.sum(10));
+      System.out.println(Hello.sum(100));
     }
   }
   
