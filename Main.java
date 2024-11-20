58822994274db97d4dd33a4d26e6a2b611252851
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-11-20 09:15:27 +0900

    four

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main {
       System.out.println(Hello.sum(10));
       System.out.println(Hello.sum(100));
       System.out.println(Hello.sum(1000));
+      System.out.println(World.factorial(3));
+      System.out.println(World.factorial(6));
+      System.out.println(World.factorial(9));
     }
   }
   
