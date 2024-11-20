ca409c9a313642549da58413a348745899d2acce
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-11-20 09:18:45 +0900

    six

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
       //  자신의 학번과 이름을 출력하라
-      System.out.println("202114073 박예은");
-      System.out.println(Hello.sum(10));
-      System.out.println(Hello.sum(100));
-      System.out.println(Hello.sum(1000));
-      System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
-      System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+      out.println("202114073 박예은");
+      out.println(Hello.sum(10));
+      out.println(Hello.sum(100));
+      out.println(Hello.sum(1000));
+      out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+      out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
     }
   }
   
